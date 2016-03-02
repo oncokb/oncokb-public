c771e4365295a4d861c86d4184e85e92ca263a92
Hongxin Zhang <jackson.zhang.828@gmail.com>
2016-03-02 14:47:50 -0500

    Color coded by levels; Shorten canvas height for 13 inch display

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -11,15 +11,35 @@ angular.module('oncokbStaticApp')
   .controller('HomeCtrl', function ($scope, $location) {
     $scope.content = {hoveredGene: "gets", hoveredCount: ''};
 
-    d3.csv('resources/files/all_genes_with_variants.csv', function (content) {
+    d3.csv('resources/files/all_genes_with_all_variants.csv', function (content) {
+      var levelColors = {
+        'LEVEL_1': '#006400',
+        'LEVEL_2A': '#32CD32',
+        'LEVEL_2B': '#98FB98',
+        'LEVEL_3A': '#FFAA00',
+        'LEVEL_3B': '#FF0000',
+        'LEVEL_4': '#8B0000',
+        //'LEVEL_R1': '#F40000',
+        //'LEVEL_R2': '#C4006F',
+        //'LEVEL_R3': '#6F08A3',
+        'Other': '#000000'
+      };
+
+      var genes = {};
+
       WordCloud(document.getElementById('wordCloud'), {
         list: content.map(function (d) {
-          return [d.gene, Math.sqrt(d.alt*2) * 5, d.alt];
+          genes[d.gene] = d;
+          return [d.gene, Math.sqrt(d.altNum*3) * 4, d.altNum];
         }),
-        fontFamily: 'tunga',
-        shape: 'Ariel',
+        fontFamily: 'Calibri',
+        shape: 'circle',
         rotateRatio: 0,
-        gridSize: '14',
+        gridSize: '8',
+        shuffle: false,
+        color: function (word) {
+          return levelColors.hasOwnProperty(genes[word].hLevel)?levelColors[genes[word].hLevel]:levelColors['Other'];
+        },
         hover: function (item, dimension, event) {
           var el = document.getElementById('canvas-hover');
           var hoverLabelElement = document.getElementById('canvas-hover-label');
