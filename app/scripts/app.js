c677a50beade68abe056bc35771488991c19e67f
Hongxin Zhang <jackson.zhang.828@gmail.com>
2017-04-19 13:42:32 -0400

    Change V544_L555insAVLVLLVIVIISLI to V544_L545insAVLVLLVIVIISLI

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -129,8 +129,8 @@ angular.module('oncokbStaticApp').run(
             }, {content: '3800 Variants'}, {content: '333 Tumor Types'}]
         };
         $rootScope.data = {
-            lastUpdate: 'April 05, 2017',
-            version: '1.9',
+            lastUpdate: 'April 19, 2017',
+            version: '1.9.patch.2',
             levelColors: {
                 '1': '#33A02C',
                 '2A': '#1F78B4',
