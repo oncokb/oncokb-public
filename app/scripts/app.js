91b8928d378cbed35ce172410f0cac075bbd0271
Hongxin Zhang <jackson.zhang.828@gmail.com>
2017-08-30 01:44:43 -0400

    Update version and upload latest data

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -139,8 +139,8 @@ angular.module('oncokbStaticApp').run(
             }, {content: '3800 Variants'}, {content: '333 Tumor Types'}]
         };
         $rootScope.data = {
-            lastUpdate: 'August 17, 2017',
-            version: '1.11',
+            lastUpdate: 'August 31, 2017',
+            version: '1.11_patch_1',
             levelColors: {
                 '1': '#33A02C',
                 '2A': '#1F78B4',
