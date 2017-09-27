0eeb1eb90cdfd69c542daeb3ddbf124164b306f5
JiaoJiao123 <jiaojiaowanghere@gmail.com>
2017-09-27 15:18:16 -0400

    replace tcga studies with msk impact

--- app/scripts/services/api.js
+++ app/scripts/services/api.js
@@ -77,9 +77,6 @@ angular.module('oncokbStaticApp')
                 return $http.get(legacyLink +
                     'mutationMapperData?hugoSymbol=' + hugoSymbol);
             },
-            getStudies: function(studies) {
-                return $http.get('http://www.cbioportal.org/api-legacy/studies');
-            },
             getpumbedArticle: function(pumbedID) {
                 return $http.get('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&retmode=json&id=' + pumbedID);
             },
