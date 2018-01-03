074cc6da822f14e7d76a6c751e52b67d967a48ad
Hongxin Zhang <hongxin@cbio.mskcc.org>
2018-01-03 18:35:42 -0500

    Update version

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -139,8 +139,8 @@ angular.module('oncokbStaticApp').run(
             }, {content: '3800 Variants'}, {content: '333 Tumor Types'}]
         };
         $rootScope.data = {
-            lastUpdate: 'October 26, 2017',
-            version: '1.12_pathch_1',
+            lastUpdate: 'January 4, 2018',
+            version: '1.12_patch_2',
             levelColors: {
                 '1': '#33A02C',
                 '2A': '#1F78B4',
