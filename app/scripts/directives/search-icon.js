2658f3efb66d97ac0678c096e331ca38ad9293aa
Hongxin Zhang <hongxin@cbio.mskcc.org>
2018-01-16 16:06:30 -0500

    Add search box in mobile mode
    Solving the issue the search box will not be hidden when click on search result

--- app/scripts/directives/search-icon.js
+++ app/scripts/directives/search-icon.js
@@ -7,12 +7,22 @@
  * # searchIcon
  */
 angular.module('oncokbStaticApp')
-    .directive('searchIcon', function() {
+    .directive('searchIcon', function($timeout) {
         return {
             templateUrl: 'views/searchIcon.html',
             restrict: 'E',
             scope: {},
             link: function postLink(scope, element, attrs) {
+                scope.data = {
+                    hideRect: false
+                };
+                scope.mouseLeave = function() {
+                    // set timeout for the animation from css.
+                    // Time should be the same with the transition time
+                    $timeout(function() {
+                        scope.data.hideRect = false;
+                    }, 500);
+                };
             }
         };
     });
