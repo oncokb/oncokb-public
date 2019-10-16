aa838d84cd77c8ce65e3652f3f3b2a7d392a2ca8
Hongxin Zhang <hongxin@cbio.mskcc.org>
2019-10-16 11:17:28 -0400

    Adjust alteration page style

---
+++ src/main/webapp/app/shared/icons/InfoIcon.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { RCTooltip } from 'rc-tooltip';
+import { DefaultTooltip } from 'cbioportal-frontend-commons';
+
+const InfoIcon: React.FunctionComponent<{
+  overlay?: React.ReactNode
+  placement?: RCTooltip.Placement
+  style?: React.CSSProperties
+  className?: string
+}> = (props) => {
+  return (
+    <DefaultTooltip
+      overlay={props.overlay}
+      placement={props.placement}
+    >
+      <span className={`fa-stack ${props.className}`} style={props.style}>
+        <i className="fa fa-circle-thin fa-stack-2x"></i>
+        <i className="fa fa-info fa-stack-1x"></i>
+      </span>
+
+    </DefaultTooltip>
+  );
+};
+export default InfoIcon;
