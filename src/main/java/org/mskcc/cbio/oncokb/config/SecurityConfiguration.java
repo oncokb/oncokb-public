92b94f455a550a1a8b5baa4102871666a85f3d17
Hongxin Zhang <hongxin@cbio.mskcc.org>
2019-10-24 18:02:08 -0400

    Include google analytics
    This solves https://github.com/oncokb/oncokb/issues/1741

--- src/main/java/org/mskcc/cbio/oncokb/config/SecurityConfiguration.java
+++ src/main/java/org/mskcc/cbio/oncokb/config/SecurityConfiguration.java
@@ -64,7 +64,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .accessDeniedHandler(problemSupport)
         .and()
             .headers()
-            .contentSecurityPolicy("default-src 'self'; frame-src 'self' data:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://storage.googleapis.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:; connect-src 'self' https://*")
+            .contentSecurityPolicy("default-src 'self'; frame-src 'self' data:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://storage.googleapis.com www.google-analytics.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: www.google-analytics.com; font-src 'self' data:; connect-src 'self' https://*")
         .and()
             .referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.NO_REFERRER_WHEN_DOWNGRADE)
         .and()
