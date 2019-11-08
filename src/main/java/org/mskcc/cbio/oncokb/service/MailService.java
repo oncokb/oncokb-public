ebb56698061fb8be2470498363c0b0b7e5690383
Hongxin Zhang <hongxin@cbio.mskcc.org>
2019-11-08 12:21:20 -0500

    Change back to html email

--- src/main/java/org/mskcc/cbio/oncokb/service/MailService.java
+++ src/main/java/org/mskcc/cbio/oncokb/service/MailService.java
@@ -79,7 +79,7 @@ public class MailService {
         context.setVariable(BASE_URL, jHipsterProperties.getMail().getBaseUrl());
         String content = templateEngine.process(templateName, context);
         String subject = messageSource.getMessage(titleKey, null, locale);
-        sendEmail(user.getEmail(), subject, content, false, false);
+        sendEmail(user.getEmail(), subject, content, false, true);
     }
 
     @Async
