36de2c2c234ced8c3da300a3d0d3acb62e1dfeb4
Hongxin Zhang <hongxin@cbio.mskcc.org>
2019-11-20 11:43:51 -0500

    Update base on feedback

--- src/main/webapp/app/config/constants.ts
+++ src/main/webapp/app/config/constants.ts
@@ -297,7 +297,7 @@ export enum LicenseType {
 export const LICENSE_TITLES: { [key: string]: string } = {
   [LicenseType.ACADEMIC]: 'Research use in an academic setting',
   [LicenseType.RESEARCH_IN_COMMERCIAL]: 'Research use in a commercial setting',
-  [LicenseType.HOSPITAL]: 'Annotation of patient reports in a hospital',
+  [LicenseType.HOSPITAL]: 'Use for patient services or reports in hospital/care setting',
   [LicenseType.COMMERCIAL]: 'Use in a commercial product'
 };
 
