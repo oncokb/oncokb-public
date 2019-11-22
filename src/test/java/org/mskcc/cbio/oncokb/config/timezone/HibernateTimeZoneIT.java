d0ac0b5bffffe70e9d0f44ab63ef4b25add8150d
Hongxin Zhang <hongxin@cbio.mskcc.org>
2019-11-22 15:35:44 -0500

    Update caching to redis from ehcache
    For distribution caching

--- src/test/java/org/mskcc/cbio/oncokb/config/timezone/HibernateTimeZoneIT.java
+++ src/test/java/org/mskcc/cbio/oncokb/config/timezone/HibernateTimeZoneIT.java
@@ -1,10 +1,12 @@
 package org.mskcc.cbio.oncokb.config.timezone;
 
 import org.mskcc.cbio.oncokb.OncokbApp;
+import org.mskcc.cbio.oncokb.RedisTestContainerExtension;
 import org.mskcc.cbio.oncokb.repository.timezone.DateTimeWrapper;
 import org.mskcc.cbio.oncokb.repository.timezone.DateTimeWrapperRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -21,6 +23,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  * Integration tests for the UTC Hibernate configuration.
  */
 @SpringBootTest(classes = OncokbApp.class)
+@ExtendWith(RedisTestContainerExtension.class)
 public class HibernateTimeZoneIT {
 
     @Autowired
--- src/test/java/org/mskcc/cbio/oncokb/web/rest/errors/ExceptionTranslatorIT.java
+++ src/test/java/org/mskcc/cbio/oncokb/web/rest/errors/ExceptionTranslatorIT.java
@@ -1,8 +1,10 @@
 package org.mskcc.cbio.oncokb.web.rest.errors;
 
+import org.mskcc.cbio.oncokb.RedisTestContainerExtension;
 import org.mskcc.cbio.oncokb.OncokbApp;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -20,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * Integration tests {@link ExceptionTranslator} controller advice.
  */
 @SpringBootTest(classes = OncokbApp.class)
+@ExtendWith(RedisTestContainerExtension.class)
 public class ExceptionTranslatorIT {
 
     @Autowired
