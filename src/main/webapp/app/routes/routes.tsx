dfc533ce5ad356ab9f9dd62ae0fdac82f9849099
jingsu <victoriasu1994@gmail.com>
2019-12-02 17:48:29 -0500

    Redirect to Home Page for 404 error

--- src/main/webapp/app/routes/routes.tsx
+++ src/main/webapp/app/routes/routes.tsx
@@ -24,7 +24,7 @@ import { AccountPage } from 'app/pages/AccountPage';
 import ActivateAccount from 'app/components/account/ActivateAccount';
 import { PasswordResetInit } from 'app/components/account/PasswordResetInit';
 import PasswordResetFinish from 'app/components/account/PasswordResetFinish';
-import PageNotFound from '../shared/error/page-not-found';
+import PageNotFound from 'app/shared/error/page-not-found';
 import DataAccessPage from 'app/pages/DataAccessPage';
 import AccountPassword from 'app/components/account/AccountPassword';
 import AdminRouts from 'app/routes/AdminRoutes';
@@ -51,78 +51,80 @@ const AppRouts = (props: {
         to={PAGE_ROUTE.ALTERATION}
       />
       <PageContainer>
-        <ErrorBoundaryRoute path={PAGE_ROUTE.LOGIN} component={Login} />
-        <ErrorBoundaryRoute path={PAGE_ROUTE.LOGOUT} component={Logout} />
-        <ErrorBoundaryRoute
-          path={PAGE_ROUTE.REGISTER}
-          component={RegisterPage}
-        />
-        <ErrorBoundaryRoute
-          path={PAGE_ROUTE.DATA_ACCESS}
-          component={DataAccessPage}
-        />
-        <ErrorBoundaryRoute
-          path={PAGE_ROUTE.CANCER_GENES}
-          component={CancerGenesPage}
-        />
-        <ErrorBoundaryRoute
-          path={PAGE_ROUTE.ACTIONABLE_GENE}
-          component={ActionableGenesPage}
-        />
-        <ErrorBoundaryRoute exact path={PAGE_ROUTE.GENE} component={GenePage} />
-        <ErrorBoundaryRoute
-          exact
-          path={PAGE_ROUTE.ALTERATION}
-          component={AlterationPage}
-        />
-        <ErrorBoundaryRoute
-          exact
-          path={PAGE_ROUTE.ALTERATION_TUMOR_TYPE}
-          component={AlterationPage}
-        />
-        <Route exact path={PAGE_ROUTE.ABOUT} component={AboutPage} />
-        <Route exact path={PAGE_ROUTE.TERMS} component={TermsPage} />
-        <Route exact path={PAGE_ROUTE.TEAM} component={TeamPage} />
-        <Route exact path={PAGE_ROUTE.NEWS} component={NewsPage} />
-        <Route exact path={PAGE_ROUTE.LEVELS} component={LevelOfEvidencePage} />
-        <ErrorBoundaryRoute
-          exact
-          path={PAGE_ROUTE.ACCOUNT_VERIFY}
-          component={ActivateAccount}
-        />
-        <ErrorBoundaryRoute
-          path={PAGE_ROUTE.ACCOUNT_PASSWORD_RESET_REQUEST}
-          component={PasswordResetInit}
-        />
-        <ErrorBoundaryRoute
-          path={PAGE_ROUTE.ACCOUNT_PASSWORD_RESET_FINISH}
-          component={PasswordResetFinish}
-        />
-        <PrivateRoute
-          authenticationStore={props.authenticationStore}
-          routing={props.routing}
-          path={PAGE_ROUTE.ADMIN}
-          component={AdminRouts}
-          hasAnyAuthorities={[AUTHORITIES.ADMIN]}
-        />
-        <PrivateRoute
-          exact
-          authenticationStore={props.authenticationStore}
-          routing={props.routing}
-          path={PAGE_ROUTE.ACCOUNT_PASSWORD}
-          component={AccountPassword}
-          hasAnyAuthorities={[AUTHORITIES.ADMIN, AUTHORITIES.USER]}
-        />
-        <PrivateRoute
-          exact
-          path={PAGE_ROUTE.ACCOUNT_SETTINGS}
-          authenticationStore={props.authenticationStore}
-          routing={props.routing}
-          component={AccountPage}
-          hasAnyAuthorities={[AUTHORITIES.ADMIN, AUTHORITIES.USER]}
-        />
+        <Switch>
+          <ErrorBoundaryRoute path={PAGE_ROUTE.LOGIN} component={Login} />
+          <ErrorBoundaryRoute path={PAGE_ROUTE.LOGOUT} component={Logout} />
+          <ErrorBoundaryRoute
+            path={PAGE_ROUTE.REGISTER}
+            component={RegisterPage}
+          />
+          <ErrorBoundaryRoute
+            path={PAGE_ROUTE.DATA_ACCESS}
+            component={DataAccessPage}
+          />
+          <ErrorBoundaryRoute
+            path={PAGE_ROUTE.CANCER_GENES}
+            component={CancerGenesPage}
+          />
+          <ErrorBoundaryRoute
+            path={PAGE_ROUTE.ACTIONABLE_GENE}
+            component={ActionableGenesPage}
+          />
+          <ErrorBoundaryRoute exact path={PAGE_ROUTE.GENE} component={GenePage} />
+          <ErrorBoundaryRoute
+            exact
+            path={PAGE_ROUTE.ALTERATION}
+            component={AlterationPage}
+          />
+          <ErrorBoundaryRoute
+            exact
+            path={PAGE_ROUTE.ALTERATION_TUMOR_TYPE}
+            component={AlterationPage}
+          />
+          <Route exact path={PAGE_ROUTE.ABOUT} component={AboutPage} />
+          <Route exact path={PAGE_ROUTE.TERMS} component={TermsPage} />
+          <Route exact path={PAGE_ROUTE.TEAM} component={TeamPage} />
+          <Route exact path={PAGE_ROUTE.NEWS} component={NewsPage} />
+          <Route exact path={PAGE_ROUTE.LEVELS} component={LevelOfEvidencePage} />
+          <ErrorBoundaryRoute
+            exact
+            path={PAGE_ROUTE.ACCOUNT_VERIFY}
+            component={ActivateAccount}
+          />
+          <ErrorBoundaryRoute
+            path={PAGE_ROUTE.ACCOUNT_PASSWORD_RESET_REQUEST}
+            component={PasswordResetInit}
+          />
+          <ErrorBoundaryRoute
+            path={PAGE_ROUTE.ACCOUNT_PASSWORD_RESET_FINISH}
+            component={PasswordResetFinish}
+          />
+          <PrivateRoute
+            authenticationStore={props.authenticationStore}
+            routing={props.routing}
+            path={PAGE_ROUTE.ADMIN}
+            component={AdminRouts}
+            hasAnyAuthorities={[AUTHORITIES.ADMIN]}
+          />
+          <PrivateRoute
+            exact
+            authenticationStore={props.authenticationStore}
+            routing={props.routing}
+            path={PAGE_ROUTE.ACCOUNT_PASSWORD}
+            component={AccountPassword}
+            hasAnyAuthorities={[AUTHORITIES.ADMIN, AUTHORITIES.USER]}
+          />
+          <PrivateRoute
+            exact
+            path={PAGE_ROUTE.ACCOUNT_SETTINGS}
+            authenticationStore={props.authenticationStore}
+            routing={props.routing}
+            component={AccountPage}
+            hasAnyAuthorities={[AUTHORITIES.ADMIN, AUTHORITIES.USER]}
+          />
+          <ErrorBoundaryRoute component={PageNotFound} />
+        </Switch>
       </PageContainer>
-      <ErrorBoundaryRoute component={PageNotFound} />
     </Switch>
   );
 };
