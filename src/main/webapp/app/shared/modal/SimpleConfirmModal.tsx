e995868a0284544836980e4ec4a49c1ca9628275
Hongxin <zhx828@users.noreply.github.com>
2020-01-04 18:49:47 -0500

    Add Premium User Tier support (#362)
    * Add ROLE_PREMIUM_USER and set the proper access in SecurityConfig
    
    * Allow admin to assign ROLE_PREMIUM authority to user

--- src/main/webapp/app/shared/modal/SimpleConfirmModal.tsx
+++ src/main/webapp/app/shared/modal/SimpleConfirmModal.tsx
@@ -1,38 +1,39 @@
-import {Button, Modal} from "react-bootstrap";
-import React from "react";
+import { Button, Modal } from 'react-bootstrap';
+import React from 'react';
 
 export const SimpleConfirmModal: React.FunctionComponent<{
-  show: boolean
-  onCancel?: () => void,
-  onConfirm?: () => void
+  title?: string;
+  body?: string;
+  show: boolean;
+  onCancel?: () => void;
+  onConfirm?: () => void;
 }> = props => {
   const onCancel = (event?: any) => {
-    if (event)
-      event.preventDefault();
-    if (props.onCancel)
-      props.onCancel()
+    if (event) event.preventDefault();
+    if (props.onCancel) props.onCancel();
   };
   return (
-    <Modal
-      show={props.show}
-      onHide={() => onCancel()}
-    >
+    <Modal show={props.show} onHide={() => onCancel()}>
       <Modal.Header closeButton>
-        <Modal.Title>Please confirm</Modal.Title>
+        <Modal.Title>
+          {props.title ? props.title : 'Please confirm'}
+        </Modal.Title>
       </Modal.Header>
-      <Modal.Body>Are you sure?</Modal.Body>
+      <Modal.Body>{props.body ? props.body : 'Are you sure?'}</Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={(event: any) => onCancel(event)}>
           Cancel
         </Button>
-        <Button variant="primary" onClick={(event: any) => {
-          event.preventDefault();
-          if (props.onConfirm)
-            props.onConfirm()
-        }}>
+        <Button
+          variant="primary"
+          onClick={(event: any) => {
+            event.preventDefault();
+            if (props.onConfirm) props.onConfirm();
+          }}
+        >
           Confirm
         </Button>
       </Modal.Footer>
     </Modal>
   );
-}
+};
