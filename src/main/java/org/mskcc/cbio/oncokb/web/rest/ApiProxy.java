21a3c62d126a8e18cf853ff457fc9147e5001127
Hongxin Zhang <hongxin@cbio.mskcc.org>
2020-02-04 21:57:21 +0800

    Count the token usage based on the http request method
    The GET will add 1 to token usage
    The POST will add the number of items on the body list

--- src/main/java/org/mskcc/cbio/oncokb/web/rest/ApiProxy.java
+++ src/main/java/org/mskcc/cbio/oncokb/web/rest/ApiProxy.java
@@ -1,5 +1,7 @@
 package org.mskcc.cbio.oncokb.web.rest;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.sun.xml.bind.v2.TODO;
 import org.mskcc.cbio.oncokb.config.application.ApplicationProperties;
 import org.mskcc.cbio.oncokb.domain.Authority;
@@ -18,6 +20,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -69,34 +72,7 @@ public class ApiProxy {
         List<String> tokenUsageCheckList = Arrays.stream(applicationProperties.getTokenUsageCheck().split(",")).map(api -> api.trim()).filter(api -> !api.isEmpty()).collect(Collectors.toList());
         Optional<String> needsToBeRecorded = tokenUsageCheckList.stream().filter(api -> request.getRequestURI().startsWith(api)).findFirst();
         if (needsToBeRecorded.isPresent()) {
-            Optional<String> userOptional = SecurityUtils.getCurrentUserLogin();
-            if (userOptional.isPresent()) {
-                List<String> tokenUsageCheckWhitelist = Arrays.stream(applicationProperties.getTokenUsageCheckWhitelist().split(",")).map(api -> api.trim()).filter(api -> !api.isEmpty()).collect(Collectors.toList());
-
-                Optional<User> user = userService.getUserWithAuthoritiesByLogin(userOptional.get());
-                Optional<UUID> uuidOptional = SecurityUtils.getCurrentUserToken();
-                if (user.isPresent() &&
-                    user.get().getAuthorities().stream().filter(authority -> authority.getName().equalsIgnoreCase(AuthoritiesConstants.ADMIN)).count() == 0 &&
-                    !tokenUsageCheckWhitelist.contains(user.get().getLogin())) {
-                    List<Token> tokenList = tokenProvider.getUserTokens(user.get());
-                    tokenList.forEach(token -> {
-                        token.setCurrentUsage(token.getCurrentUsage() + 1);
-                        tokenService.save(token);
-                        if (uuidOptional.isPresent() && uuidOptional.get().equals(token.getToken())) {
-                            TokenStats tokenStats = new TokenStats();
-                            tokenStats.setToken(token);
-
-                            String ipAddress = request.getHeader(IP_HEADER);
-                            if (ipAddress == null) {
-                                tokenStats.setAccessIp(request.getRemoteAddr());
-                            }
-                            tokenStats.setAccessTime(Instant.now());
-                            tokenStats.setResource(request.getMethod() + " " + request.getRequestURI());
-                            tokenStatsService.save(tokenStats);
-                        }
-                    });
-                }
-            }
+            updateTokenStats(body, method, request);
         }
         HttpHeaders httpHeaders = apiProxyService.prepareHttpHeaders(request.getContentType());
         RestTemplate restTemplate = new RestTemplate();
@@ -104,6 +80,49 @@ public class ApiProxy {
         return restTemplate.exchange(uri, method, new HttpEntity<>(body, httpHeaders), String.class).getBody();
     }
 
+
+    @Async
+    public void updateTokenStats(String body, HttpMethod method, HttpServletRequest request) {
+        Optional<String> userOptional = SecurityUtils.getCurrentUserLogin();
+        if (userOptional.isPresent()) {
+            List<String> tokenUsageCheckWhitelist = Arrays.stream(applicationProperties.getTokenUsageCheckWhitelist().split(",")).map(api -> api.trim()).filter(api -> !api.isEmpty()).collect(Collectors.toList());
+
+            Optional<User> user = userService.getUserWithAuthoritiesByLogin(userOptional.get());
+            Optional<UUID> uuidOptional = SecurityUtils.getCurrentUserToken();
+            if (user.isPresent() &&
+                user.get().getAuthorities().stream().filter(authority -> authority.getName().equalsIgnoreCase(AuthoritiesConstants.ADMIN)).count() == 0 &&
+                !tokenUsageCheckWhitelist.contains(user.get().getLogin())) {
+                List<Token> tokenList = tokenProvider.getUserTokens(user.get());
+                int usageCount = 1;
+                if (method != null && method.equals(HttpMethod.POST)) {
+                    try {
+                        Gson gson = new GsonBuilder().create();
+                        Object[] postRequest = gson.fromJson(body, Object[].class);
+                        usageCount = postRequest.length;
+                    }catch (Exception e) {
+                        // For any reason the request cannot be parsed to a list, we should ignore it and count it as 1
+                    }
+                }
+                int finalUsageCount = usageCount;
+                tokenList.forEach(token -> {
+                    token.setCurrentUsage(token.getCurrentUsage() + finalUsageCount);
+                    tokenService.save(token);
+                    if (uuidOptional.isPresent() && uuidOptional.get().equals(token.getToken())) {
+                        TokenStats tokenStats = new TokenStats();
+                        tokenStats.setToken(token);
+
+                        String ipAddress = request.getHeader(IP_HEADER);
+                        if (ipAddress == null) {
+                            tokenStats.setAccessIp(request.getRemoteAddr());
+                        }
+                        tokenStats.setAccessTime(Instant.now());
+                        tokenStats.setResource(request.getMethod() + " " + request.getRequestURI());
+                        tokenStatsService.save(tokenStats);
+                    }
+                });
+            }
+        }
+    }
     //    @RequestMapping(value = "/private/utils/dataRelease/sqlDump",
 //        produces = {"application/zip"},
 //        method = RequestMethod.GET)
