5e6eeac7eaa098e0fe77f379afe7c012d381e0ba
Hongxin Zhang <hongxin@cbio.mskcc.org>
2020-04-21 22:18:54 -0400

    Change Data Access page to API Access

--- src/main/webapp/app/components/Header.tsx
+++ src/main/webapp/app/components/Header.tsx
@@ -34,7 +34,7 @@ class Header extends React.Component<IHeaderProps> {
     { title: 'Levels of Evidence', link: 'levels' },
     { title: 'Actionable Genes', link: 'actionableGenes' },
     { title: 'Cancer Genes', link: 'cancerGenes' },
-    { title: 'Data Access', link: 'dataAccess' },
+    { title: 'API Access', link: 'apiAccess' },
     { title: 'About', link: 'about' },
     { title: 'Team', link: 'team' },
     { title: 'News', link: 'news' },
