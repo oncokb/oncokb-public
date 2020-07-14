dd6c6a4c338f32762e99abddddd1b443b12190e6
Yifu Yao <yveyao@outlook.com>
2020-07-14 09:19:47 -0500

    Add local dev config

--- src/main/webapp/app/shared/api/clientInstance.ts
+++ src/main/webapp/app/shared/api/clientInstance.ts
@@ -1,5 +1,6 @@
 import API from './generated/API';
+import { dev } from '../../config/constants';
 
-const client = new API();
+const client = new API(dev);
 
 export default client;
--- src/main/webapp/app/shared/api/oncokbClientInstance.ts
+++ src/main/webapp/app/shared/api/oncokbClientInstance.ts
@@ -1,5 +1,8 @@
 import OncoKbAPI from './generated/OncoKbAPI';
+import { dev } from '../../config/constants';
 
-const client = new OncoKbAPI('api/v1');
+let preURL = '';
+if (dev.length > 0) preURL = dev + '/';
+const client = new OncoKbAPI(preURL + 'api/v1');
 
 export default client;
