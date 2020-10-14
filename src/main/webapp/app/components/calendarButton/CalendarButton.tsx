790f2d4b093a35a2e65c545d8168f4859ac69eee
Hongxin Zhang <hongxin@cbio.mskcc.org>
2020-10-14 17:35:41 -0400

    Add a header on token extention component to clarify the use case

--- src/main/webapp/app/components/calendarButton/CalendarButton.tsx
+++ src/main/webapp/app/components/calendarButton/CalendarButton.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   InputGroup,
   Button,
   Dropdown,
   DropdownButton,
   Row,
+  Form,
+  Col,
+  Container,
 } from 'react-bootstrap';
 import classnames from 'classnames';
 import { getMomentInstance } from 'app/shared/utils/Utils';
@@ -24,7 +27,14 @@ type CalendarButtonProps = {
   afterChangeDate: (newDate: string) => void;
 };
 
+enum ExtensionType {
+  FROM_TODAY = 'Today',
+  FROM_EXPIRATION = 'Expiration Day',
+}
+
 export const CalendarButton: React.FunctionComponent<CalendarButtonProps> = props => {
+  const [extensionType, setExtensionType] = useState(ExtensionType.FROM_TODAY);
+
   let selectedDay: Date;
 
   function handleDayChange(day: any) {
@@ -71,48 +81,82 @@ export const CalendarButton: React.FunctionComponent<CalendarButtonProps> = prop
         title={<i className={classnames('fa fa-calendar')}></i>}
         id="time-entend-dropdown"
       >
-        {extendSchedule.map((val, index) => {
-          return (
-            <Dropdown.Item
-              eventKey={`${index}`}
-              style={{ textAlign: 'center' }}
-              onClick={() => {
-                props.afterChangeDate(
-                  getMomentInstance(props.currentDate)
-                    .add(val.amount, val.unit)
-                    .format(APP_LOCAL_DATETIME_FORMAT_Z_FORCE)
-                );
-              }}
+        <Container>
+          <div>
+            <Row>
+              <Col>Extend after</Col>
+            </Row>
+            <Form.Group as={Row}>
+              <Col>
+                <Form.Check
+                  label={ExtensionType.FROM_TODAY}
+                  type="radio"
+                  onClick={() => setExtensionType(ExtensionType.FROM_TODAY)}
+                  name="extension-choice"
+                  id="extension-type-by-today"
+                  defaultChecked
+                />
+                <Form.Check
+                  label={ExtensionType.FROM_EXPIRATION}
+                  type="radio"
+                  onClick={() =>
+                    setExtensionType(ExtensionType.FROM_EXPIRATION)
+                  }
+                  name="extension-choice"
+                  id="extension-type-by-expiration"
+                />
+              </Col>
+            </Form.Group>
+          </div>
+          <Dropdown.Divider />
+          {extendSchedule.map((val, index) => {
+            return (
+              <Dropdown.Item
+                key={`calender-quick-selection-${index}`}
+                eventKey={`${index}`}
+                style={{ textAlign: 'center' }}
+                onClick={() => {
+                  const momentInstance =
+                    extensionType === ExtensionType.FROM_TODAY
+                      ? moment()
+                      : getMomentInstance(props.currentDate);
+                  props.afterChangeDate(
+                    momentInstance
+                      .add(val.amount, val.unit)
+                      .format(APP_LOCAL_DATETIME_FORMAT_Z_FORCE)
+                  );
+                }}
+              >
+                {pluralize(val.unitText, val.amount, true)}
+              </Dropdown.Item>
+            );
+          })}
+          <Dropdown.Divider />
+          <DayPickerInput
+            onDayChange={handleDayChange}
+            inputProps={{
+              style: {
+                textAlign: 'center',
+                border: 'none',
+              },
+            }}
+            style={{ margin: '0 auto' }}
+            formatDate={MomentLocaleUtils.formatDate}
+            parseDate={MomentLocaleUtils.parseDate}
+            placeholder={`Expires on: ${getMomentInstance(
+              props.currentDate
+            ).format(APP_LOCAL_DATE_FORMAT)}`}
+          />
+          <Row className={'mt-1'}>
+            <Button
+              size={'sm'}
+              style={{ margin: '0 auto', zIndex: 0 }}
+              onClick={handleSelectedDay}
             >
-              {pluralize(val.unitText, val.amount, true)}
-            </Dropdown.Item>
-          );
-        })}
-        <Dropdown.Divider />
-        <DayPickerInput
-          onDayChange={handleDayChange}
-          inputProps={{
-            style: {
-              textAlign: 'center',
-              border: 'none',
-            },
-          }}
-          style={{ margin: '0 auto' }}
-          formatDate={MomentLocaleUtils.formatDate}
-          parseDate={MomentLocaleUtils.parseDate}
-          placeholder={`Expires on: ${getMomentInstance(
-            props.currentDate
-          ).format(APP_LOCAL_DATE_FORMAT)}`}
-        />
-        <Row className={'mt-1'}>
-          <Button
-            size={'sm'}
-            style={{ margin: '0 auto', zIndex: 0 }}
-            onClick={handleSelectedDay}
-          >
-            Set
-          </Button>
-        </Row>
+              Set
+            </Button>
+          </Row>
+        </Container>
       </DropdownButton>
     </DefaultTooltip>
   );
