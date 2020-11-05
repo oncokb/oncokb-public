776436e3b95512a5c86f87151d256e730d092eab
Yifu Yao <yveyao@outlook.com>
2020-11-05 17:22:08 -0600

    oncokb usage page update

--- src/main/webapp/app/pages/usageAnalysisPage/UsageDetailsTable.tsx
+++ src/main/webapp/app/pages/usageAnalysisPage/UsageDetailsTable.tsx
@@ -1,15 +1,24 @@
 import OncoKBTable from 'app/components/oncokbTable/OncoKBTable';
 import { UsageRecord } from 'app/shared/api/generated/API';
 import { filterByKeyword } from 'app/shared/utils/Utils';
+import autobind from 'autobind-decorator';
+import _ from 'lodash';
 import { observable } from 'mobx';
 import { observer } from 'mobx-react';
 import React from 'react';
-import { Dropdown, DropdownButton } from 'react-bootstrap';
+import {
+  Dropdown,
+  DropdownButton,
+  Row,
+  ToggleButton,
+  ToggleButtonGroup,
+} from 'react-bootstrap';
 
 type IUsageDetailsTableProps = {
   data: Map<string, UsageRecord[]>;
   loadedData: boolean;
   dropdownList: string[];
+  defaultResourcesType: number;
 };
 
 @observer
@@ -18,6 +27,12 @@ export default class UsageDetailsTable extends React.Component<
   {}
 > {
   @observable dropdownValue = 'All';
+  @observable resourcesTypeToggleValue = this.props.defaultResourcesType;
+
+  @autobind
+  handleResourcesTypeToggleChange(value: any) {
+    this.resourcesTypeToggleValue = value;
+  }
 
   render() {
     const dropdown: any = [];
@@ -29,18 +44,29 @@ export default class UsageDetailsTable extends React.Component<
           dropdown.push(<Dropdown.Item eventKey={key}>{key}</Dropdown.Item>);
         });
     }
-
     return (
       <>
         {this.props.loadedData ? (
-          <DropdownButton
-            className="mt-2"
-            id="dropdown-basic-button"
-            title={this.dropdownValue}
-            onSelect={(evt: any) => (this.dropdownValue = evt)}
-          >
-            {dropdown}
-          </DropdownButton>
+          <Row className="mt-2">
+            <DropdownButton
+              className="ml-3"
+              id="dropdown-basic-button"
+              title={this.dropdownValue}
+              onSelect={(evt: any) => (this.dropdownValue = evt)}
+            >
+              {dropdown}
+            </DropdownButton>
+            <ToggleButtonGroup
+              className="ml-2"
+              type="radio"
+              name="resources-type-options"
+              defaultValue={this.props.defaultResourcesType}
+              onChange={this.handleResourcesTypeToggleChange}
+            >
+              <ToggleButton value={1}>All Resources</ToggleButton>
+              <ToggleButton value={2}>Only Public Resources</ToggleButton>
+            </ToggleButtonGroup>
+          </Row>
         ) : (
           <DropdownButton
             className="mt-2"
@@ -50,11 +76,24 @@ export default class UsageDetailsTable extends React.Component<
           ></DropdownButton>
         )}
         <OncoKBTable
-          data={this.props.data.get(this.dropdownValue) || []}
+          data={
+            this.resourcesTypeToggleValue === 1
+              ? this.props.data.get(this.dropdownValue) || []
+              : _.filter(this.props.data.get(this.dropdownValue), function (
+                  usage
+                ) {
+                  return !usage.resource.includes('/private/');
+                }) || []
+          }
           columns={[
             {
               id: 'resource',
-              Header: <span>Resource</span>,
+              Header:
+                this.resourcesTypeToggleValue === 1 ? (
+                  <span>Resource</span>
+                ) : (
+                  <span>Resource(only public)</span>
+                ),
               accessor: 'resource',
               minWidth: 200,
               onFilter: (data: UsageRecord, keyword) =>
