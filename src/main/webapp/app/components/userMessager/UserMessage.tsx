4eb5667c794a86004386d60978be76190747f670
Hongxin <hongxin@cbio.mskcc.org>
2021-05-12 10:12:15 -0400

    Add job banner back
    Signed-off-by: Hongxin <hongxin@cbio.mskcc.org>

--- src/main/webapp/app/components/userMessager/UserMessage.tsx
+++ src/main/webapp/app/components/userMessager/UserMessage.tsx
@@ -36,7 +36,7 @@ if (
     // UNIQUE ID IS IMPORTANT B/C WE REMEMBER A MESSAGE HAS BEEN SHOWN
     // BASED ON USERS LOCALSTORAGE
     {
-      dateEnd: 1619064000000,
+      dateEnd: 100000000000000,
       content: `
         <div>
           <span>We have a new Software Engineer position open. Come and join us! </span>
