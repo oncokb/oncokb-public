9d104eb2a77169b8d9d2ccb02ecaf1ebb74b6900
benzuzu <22bx01@stu.cpsk12.org>
2021-06-22 16:50:24 -0400

    removed UserRepository list generation

--- src/main/java/org/mskcc/cbio/oncokb/repository/UserRepository.java
+++ src/main/java/org/mskcc/cbio/oncokb/repository/UserRepository.java
@@ -24,9 +24,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findOneByActivationKey(String activationKey);
 
     List<User> findAllByActivatedIsFalseAndActivationKeyIsNotNullAndCreatedDateBefore(Instant dateTime);
-
-    List<User> findAllByActivatedIsFalseAndActivationKeyIsNullAndCreatedDateAfter(Instant dateTime);
-
+    
     @Query("select user from User user where user.activated=true and user not in (select token.user from Token token)")
     List<User> findAllActivatedWithoutTokens();
 
