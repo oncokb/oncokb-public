5335e0e5ef3d19d06d7d9ccad5dd365668d68918
Calvin Lu <lucalvin339@gmail.com>
2021-12-29 19:07:02 -0500

    Proceed with method when db-read-only is disabled

--- src/main/java/org/mskcc/cbio/oncokb/aop/api/APIConditionallyEnabledAspect.java
+++ src/main/java/org/mskcc/cbio/oncokb/aop/api/APIConditionallyEnabledAspect.java
@@ -1,6 +1,6 @@
 package org.mskcc.cbio.oncokb.aop.api;
 
-import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.mskcc.cbio.oncokb.config.application.ApplicationProperties;
@@ -20,11 +20,13 @@ public class APIConditionallyEnabledAspect {
     }
 
     @Around("@annotation(APIConditionallyEnabled)")
-    public void apiConditionallyEnabled(JoinPoint jp, APIConditionallyEnabled APIConditionallyEnabled) throws Throwable {
+    public void apiConditionallyEnabled(ProceedingJoinPoint jp, APIConditionallyEnabled APIConditionallyEnabled) throws Throwable {
         // The logic for the disabled endpoint will not run when a method has the disableEndpoint annotation
         // and in readonly mode.
         if(this.applicationProperties.getDbReadOnly() == true){
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "This endpoint is disabled");
+        }else{
+            jp.proceed();
         }
     }
 }
