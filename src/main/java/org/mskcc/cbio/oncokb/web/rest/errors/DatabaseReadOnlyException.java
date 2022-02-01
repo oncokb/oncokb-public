bbf56dfc5ad8b3669ccb6fabd847fdf4ec507cf0
Calvin Lu <lucalvin339@gmail.com>
2022-02-01 12:23:43 -0500

    Add exception handler in ExceptionTranslator.java

--- src/main/java/org/mskcc/cbio/oncokb/web/rest/errors/DatabaseReadOnlyException.java
+++ src/main/java/org/mskcc/cbio/oncokb/web/rest/errors/DatabaseReadOnlyException.java
@@ -1,6 +1,13 @@
 package org.mskcc.cbio.oncokb.web.rest.errors;
 
-public class DatabaseReadOnlyException extends RuntimeException{
+import org.zalando.problem.AbstractThrowableProblem;
+import org.zalando.problem.Status;
 
-    public DatabaseReadOnlyException() {}
+public class DatabaseReadOnlyException extends AbstractThrowableProblem{
+
+    private static final long serialVersionUID = 1L;
+
+    public DatabaseReadOnlyException() {
+        super(ErrorConstants.SERVICE_UNAVAILABLE, "Service is temporarily unavailable", Status.SERVICE_UNAVAILABLE);
+    }
 }
