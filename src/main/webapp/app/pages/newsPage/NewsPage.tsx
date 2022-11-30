5e287b4f6f83f5086c09f6893598efb185055777
Hongxin <jackson.zhang.828@gmail.com>
2022-11-30 09:01:13 -0600

    Update NewsPage.tsx

--- src/main/webapp/app/pages/newsPage/NewsPage.tsx
+++ src/main/webapp/app/pages/newsPage/NewsPage.tsx
@@ -20,10 +20,11 @@ import { Linkout } from 'app/shared/links/Linkout';
 import { RouterStore } from 'mobx-react-router';
 import { scrollWidthOffsetInNews } from 'app/shared/utils/Utils';
 import { inject, observer } from 'mobx-react';
-import indexStyles from 'app/index.module.scss';
 import { Version } from 'app/pages/LevelOfEvidencePage';
 import OptimizedImage from 'app/shared/image/OptimizedImage';
 import { NewlyAddedGenesListItem } from 'app/pages/newsPage/NewlyAddedGenesListItem';
+import { SopPageLink } from 'app/shared/utils/UrlUtils';
+
 @inject('routing')
 @observer
 export default class NewsPage extends React.Component<{
@@ -109,10 +110,10 @@ export default class NewsPage extends React.Component<{
                   In addition to monthly data releases, OncoKB will now release
                   data following any relevant new or updated FDA-approvals or
                   changes to tumor-type specific NCCN Guidelines (in accordance
-                  with the procedures outlined in the OncoKB{' '}
-                  <Linkout link={SOP_LINK}>{SOP_LINK}</Linkout>). This ensures
-                  the most relevant clinical implications are incorporated into
-                  the API as soon as possible.
+                  with the procedures outlined in the{' '}
+                  <SopPageLink version={2.2} />
+                  ). This ensures the most relevant clinical implications are
+                  incorporated into the API as soon as possible.
                 </li>
               </ul>
             </NewsList>
