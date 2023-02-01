9d0629202d7361e5074c479e478a7bf74e3e1a82
Hongxin <jackson.zhang.828@gmail.com>
2023-01-31 20:23:41 -0600

    Customize news for data v4.1
    We want to include indentation to changed related to Tucatinib + Trastuzumab

--- src/main/webapp/app/pages/newsPage/NewsPage.tsx
+++ src/main/webapp/app/pages/newsPage/NewsPage.tsx
@@ -24,6 +24,8 @@ import { Version } from 'app/pages/LevelOfEvidencePage';
 import OptimizedImage from 'app/shared/image/OptimizedImage';
 import { NewlyAddedGenesListItem } from 'app/pages/newsPage/NewlyAddedGenesListItem';
 import { SopPageLink } from 'app/shared/utils/UrlUtils';
+import { Row } from 'react-bootstrap';
+import { FdaApprovalLink } from 'app/pages/newsPage/Links';
 
 @inject('routing')
 @observer
@@ -81,7 +83,116 @@ export default class NewsPage extends React.Component<{
             <CitationText />
           </div>
           <div className="mt-2">
-            <NewsList date={'02012023'} />
+            <NewsList date={'02012023'}>
+              <ul>
+                <li>
+                  <NewlyAddedGenesListItem
+                    sort
+                    genes={['ALDH2', 'ADHFE1', 'BTG2', 'MAP4K4', 'PRPF8']}
+                  />
+                </li>
+                <li>
+                  <span>
+                    To better align with the FDA drug label, all KIT oncogenic
+                    mutations in GIST are now level 1 in association with
+                    Imatinib, Sunitinib, Ripretinib, and Regorafenib.
+                    Previously, only known sensitizing KIT exon 9, 11, 13, 14,
+                    17 and 18 alterations were considered level 1 in this
+                    indication.
+                  </span>
+                </li>
+                <li>
+                  <span>
+                    With the recent FDA approval of Tucatinib + Trastuzumab for
+                    patients with RAS wildtype, HER2-positive colorectal cancer
+                    the following OncoKB changes have been made:
+                  </span>
+                  <ul>
+                    <li>
+                      <span>
+                        Changed annotation
+                        <Row className={'overflow-auto'}>
+                          <table className="table">
+                            <thead>
+                              <tr>
+                                <th>Gene</th>
+                                <th>Mutation</th>
+                                <th>Cancer Type</th>
+                                <th>Drug(s)</th>
+                                <th>Previous Level</th>
+                                <th>Current Level</th>
+                                <th>Reason</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              <tr>
+                                <td>ERBB2</td>
+                                <td>Amplification</td>
+                                <td>Colorectal Cancer</td>
+                                <td>Tucatinib + Trastuzumab</td>
+                                <td>2</td>
+                                <td>1</td>
+                                <td>
+                                  <FdaApprovalLink
+                                    approval={
+                                      'tucatinib + trastuzumab in colorectal cancer'
+                                    }
+                                    link={
+                                      'https://www.fda.gov/drugs/resources-information-approved-drugs/fda-grants-accelerated-approval-tucatinib-trastuzumab-colorectal-cancer'
+                                    }
+                                  />
+                                </td>
+                              </tr>
+                            </tbody>
+                          </table>
+                        </Row>
+                      </span>
+                    </li>
+                    <li>
+                      <span>
+                        Updated therapeutic implications - addition of therapies
+                        for variants with a level of evidence
+                        <Row className={'overflow-auto'}>
+                          <table className="table">
+                            <thead>
+                              <tr>
+                                <th>Gene</th>
+                                <th>Mutation</th>
+                                <th>Cancer Type</th>
+                                <th>Current Level of Evidence</th>
+                                <th>Drug(s) Already in {ONCOKB_TM}</th>
+                                <th>Newly Added Drug(s)</th>
+                                <th>Evidence</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              <tr>
+                                <td>KRAS, NRAS</td>
+                                <td>Oncogenic Mutations</td>
+                                <td>Colorectal Cancer</td>
+                                <td>R1</td>
+                                <td>Cetuximab, Panitumumab</td>
+                                <td>Tucatinib + Trastuzumab</td>
+                                <td>
+                                  <FdaApprovalLink
+                                    link={
+                                      'https://www.fda.gov/drugs/resources-information-approved-drugs/fda-grants-accelerated-approval-tucatinib-trastuzumab-colorectal-cancer'
+                                    }
+                                    approval={
+                                      'tucatinib + trastuzumab in colorectal cancer'
+                                    }
+                                  />
+                                </td>
+                              </tr>
+                            </tbody>
+                          </table>
+                        </Row>
+                      </span>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </NewsList>
             <NewsList date={'01052023'} />
             <NewsList date={'12222022'} />
             <NewsList date={'12132022'} />
