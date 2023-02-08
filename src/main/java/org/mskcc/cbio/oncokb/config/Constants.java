f703863c90e6caef1054fb1d2ca9716c3fb774e6
scarrero4660 <50347793+scarrero4660@users.noreply.github.com>
2023-02-08 10:08:13 -0500

    delete from constants file

--- src/main/java/org/mskcc/cbio/oncokb/config/Constants.java
+++ src/main/java/org/mskcc/cbio/oncokb/config/Constants.java
@@ -39,9 +39,6 @@ public final class Constants {
     public static final String TOKEN_STATS_STORAGE_FILE_PREFIX = "public-website/token-usage/token-stats_";
 
     public static final String ONCOKB_TM = "OncoKB™";
-
-    public static final String VALIDATION_ERROR = "Validation failed";
-    public static final String TESTING_TOKEN = "faketoken";
     
     private Constants() {
     }
