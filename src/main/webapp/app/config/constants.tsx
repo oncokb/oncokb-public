aeff09cb9822df10cc14f9b28c6f2d725470ed8a
Calvin Lu <59149377+calvinlu3@users.noreply.github.com>
2023-08-31 16:46:00 -0400

    Update news for v4.8 (#1017)

--- src/main/webapp/app/config/constants.tsx
+++ src/main/webapp/app/config/constants.tsx
@@ -779,6 +779,7 @@ export type DataRelease = {
 };
 
 export const DATA_RELEASES: DataRelease[] = [
+  { date: '09012023', version: 'v4.8' },
   { date: '07282023', version: 'v4.7' },
   { date: '07122023', version: 'v4.6' },
   { date: '05192023', version: 'v4.5' },
