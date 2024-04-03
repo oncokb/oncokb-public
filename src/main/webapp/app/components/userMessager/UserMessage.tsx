f75109d264b919526e386fe5f9bd7abe611323bf
Hongxin <5400599+zhx828@users.noreply.github.com>
2024-04-03 13:58:37 -0400

    Update UserMessage.tsx

--- src/main/webapp/app/components/userMessager/UserMessage.tsx
+++ src/main/webapp/app/components/userMessager/UserMessage.tsx
@@ -121,7 +121,7 @@ if (
       dateEnd: 1712808000000,
       content: (
         <div>
-          Join us at AACR 2024! Visit our poster session (Sat 4/8, 1:30PM PDT,
+          Join us at AACR 2024! Visit our poster session (Mon 4/8, 1:30PM PDT,
           Section 36, Poster 3544) to learn more about OncoKB and meet some of
           our team members. See you there!
         </div>
