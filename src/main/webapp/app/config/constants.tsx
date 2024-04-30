d53e0bc2540ae7aaf5d4cf8caca9f309f518ae01
Calvin Lu <59149377+calvinlu3@users.noreply.github.com>
2024-04-30 16:14:51 -0400

    Update release date and screenshot

--- src/main/webapp/app/config/constants.tsx
+++ src/main/webapp/app/config/constants.tsx
@@ -798,7 +798,7 @@ export type DataRelease = {
 };
 
 export const DATA_RELEASES: DataRelease[] = [
-  { date: '04302024', version: 'v4.16' },
+  { date: '05012024', version: 'v4.16' },
   { date: '03212024', version: 'v4.15' },
   { date: '02082024', version: 'v4.14' },
   { date: '12212023', version: 'v4.13' },
