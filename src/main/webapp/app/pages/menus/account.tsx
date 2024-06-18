f722789c0cd9a4d2bc28484731b30020ad55ba7c
Calvin Lu <59149377+calvinlu3@users.noreply.github.com>
2024-06-18 16:35:40 -0400

    Center align navbar text and remove account text for screen less than 1500px (#1127)

--- src/main/webapp/app/pages/menus/account.tsx
+++ src/main/webapp/app/pages/menus/account.tsx
@@ -55,13 +55,14 @@ const AccountMenuItems: React.FunctionComponent<{}> = () => (
 export default class AccountMenu extends React.Component<{
   isAuthenticated: boolean;
   isAdmin: boolean;
+  showAccountText?: boolean;
 }> {
   render() {
     return (
       <Dropdown as={NavItem}>
         <Dropdown.Toggle id={'account-menu'} as={NavLink}>
           <i className={'fa fa-user-o mr-1'} />
-          {PAGE_TITLE.ACCOUNT}
+          {this.props.showAccountText ? PAGE_TITLE.ACCOUNT : undefined}
         </Dropdown.Toggle>
         <Dropdown.Menu alignRight={true}>
           {this.props.isAuthenticated ? (
