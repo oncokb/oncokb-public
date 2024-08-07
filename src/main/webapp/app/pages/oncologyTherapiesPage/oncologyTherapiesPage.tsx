7696fc887c8be0e25e9b16de1ff2d4ec586ddce6
Calvin Lu <59149377+calvinlu3@users.noreply.github.com>
2024-08-07 17:08:30 -0400

    Update onc tx page

--- src/main/webapp/app/pages/oncologyTherapiesPage/oncologyTherapiesPage.tsx
+++ src/main/webapp/app/pages/oncologyTherapiesPage/oncologyTherapiesPage.tsx
@@ -476,7 +476,7 @@ const OncologyTherapiesPage: React.FunctionComponent<{}> = props => {
                 'https://www.fda.gov/drugs/resources-information-approved-drugs/oncology-cancer-hematologic-malignancies-approval-notifications'
               }
             >
-              Content current as of 6/19/2024
+              Content current as of 8/5/2024
             </Linkout>
           </div>
           <div>
