99e688361e601cdd2b238236b3f1df260697bb1f
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2024-10-07 16:25:34 -0400

    User is granted API access on approval (#1163)

--- src/main/webapp/app/shared/modal/UserStatusModal.tsx
+++ src/main/webapp/app/shared/modal/UserStatusModal.tsx
@@ -1,23 +1,47 @@
 import { DefaultTooltip } from 'cbioportal-frontend-commons';
 import React from 'react';
-import { Button, Modal } from 'react-bootstrap';
+import { Button, Modal, Alert } from 'react-bootstrap';
 import { UserDTO } from '../api/generated/API';
+import { AUTHORITIES } from 'app/config/constants';
 
 type UserStatusModalProps = {
   user: UserDTO | undefined;
   show: boolean;
   onCancel: () => void;
-  onConfirm: (sendEmail: boolean) => void;
+  onConfirm: (sendEmail: boolean, authorities: string[]) => void;
 };
 
 export class UserStatusModal extends React.Component<UserStatusModalProps> {
   render() {
+    const isRequestingApiAccess =
+      !this.props.user?.activated &&
+      this.props.user?.additionalInfo?.apiAccessRequest?.requested;
+    const authorities = [...(this.props.user?.authorities ?? [])];
+    if (isRequestingApiAccess && !authorities.includes(AUTHORITIES.API)) {
+      authorities.push(AUTHORITIES.API);
+    }
     return (
       <Modal show={this.props.show} onHide={this.props.onCancel}>
         <Modal.Header closeButton>
           <Modal.Title>Update User Status</Modal.Title>
         </Modal.Header>
         <Modal.Body>
+          {isRequestingApiAccess && (
+            <Alert variant="warning">
+              <p>
+                This user is requesting API access with the following
+                justification:
+              </p>
+              <p>
+                "
+                {
+                  this.props.user?.additionalInfo?.apiAccessRequest
+                    ?.justification
+                }
+                "
+              </p>
+            </Alert>
+          )}
           Are you sure to{' '}
           {this.props.user?.activated ? 'deactivate' : 'activate'} the user?
         </Modal.Body>
@@ -25,7 +49,10 @@ export class UserStatusModal extends React.Component<UserStatusModalProps> {
           <Button variant="secondary" onClick={this.props.onCancel}>
             Close
           </Button>
-          <Button variant="primary" onClick={() => this.props.onConfirm(true)}>
+          <Button
+            variant="primary"
+            onClick={() => this.props.onConfirm(true, authorities)}
+          >
             Update
           </Button>
           {!this.props.user?.activated ? (
@@ -37,7 +64,7 @@ export class UserStatusModal extends React.Component<UserStatusModalProps> {
             >
               <Button
                 variant="primary"
-                onClick={() => this.props.onConfirm(false)}
+                onClick={() => this.props.onConfirm(false, authorities)}
               >
                 Silent Update
               </Button>
