0c976515719b1fcb3cdb5e39a4de7ba1329d7b0a
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2025-07-11 16:35:12 -0400

    disable rate limit

--- src/main/java/org/mskcc/cbio/oncokb/config/RateLimitConfiguration.java
+++ src/main/java/org/mskcc/cbio/oncokb/config/RateLimitConfiguration.java
@@ -7,16 +7,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class RateLimitConfiguration implements WebMvcConfigurer {
+  private final RateLimitInterceptor interceptor;
 
-    private final RateLimitInterceptor interceptor;
+  public RateLimitConfiguration(RateLimitInterceptor interceptor) {
+    this.interceptor = interceptor;
+  }
 
-    public RateLimitConfiguration(RateLimitInterceptor interceptor) {
-        this.interceptor = interceptor;
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(interceptor)
-            .addPathPatterns("/api/tokens/**");
-    }
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    //registry.addinterceptor(interceptor)
+    //.addpathpatterns("/api/tokens/**");
+  }
 }
