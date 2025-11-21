859967d27e9dfe1db63866e327d859ad40963726
bprize15 <bprize15@gmail.com>
2025-11-21 15:16:21 -0500

    fix casing for clinical implications for this biomarker tab

--- src/main/webapp/app/pages/somaticGermlineAlterationPage/SomaticGermlineAlterationPage.tsx
+++ src/main/webapp/app/pages/somaticGermlineAlterationPage/SomaticGermlineAlterationPage.tsx
@@ -650,7 +650,7 @@ export class SomaticGermlineAlterationPage extends React.Component<
                       </>
                     )}
                   <MiniNavBarHeader id="clinical-implications">
-                    Clinical Implications For This Biomarker
+                    Clinical Implications for this Biomarker
                   </MiniNavBarHeader>
                   <SomaticGermlineAlterationView
                     appStore={this.props.appStore}
