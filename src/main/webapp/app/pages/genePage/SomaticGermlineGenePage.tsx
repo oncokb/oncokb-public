3b92b2b89f34e78ea560a3835e31ed961f528fa7
Calvin Lu <59149377+calvinlu3@users.noreply.github.com>
2025-11-24 08:21:26 -0500

    Show mutation effect when no pathogenicity (#1273)

--- src/main/webapp/app/pages/genePage/SomaticGermlineGenePage.tsx
+++ src/main/webapp/app/pages/genePage/SomaticGermlineGenePage.tsx
@@ -707,6 +707,9 @@ export default class SomaticGermlineGenePage extends React.Component<
                                       this.store.uniqPathogenicity
                                     }
                                     oncogenicities={this.store.uniqOncogenicity}
+                                    mutationEffects={
+                                      this.store.uniqMutationEffect
+                                    }
                                     geneNumber={this.store.geneNumber.result}
                                   />
                                   <If
