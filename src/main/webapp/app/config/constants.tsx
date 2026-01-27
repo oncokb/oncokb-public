a5c6ddde286ad3103fcc3e8a5ea6d67bfa4fc33b
Calvin Lu <59149377+calvinlu3@users.noreply.github.com>
2026-01-27 11:55:18 -0500

    Support germline changes

--- src/main/webapp/app/config/constants.tsx
+++ src/main/webapp/app/config/constants.tsx
@@ -2,8 +2,9 @@ import { Gene, OncoKBInfo } from 'app/shared/api/generated/OncoKbAPI';
 import {
   Alteration,
   GeneNumber,
+  GermlineVariantAnnotation,
   MainNumber,
-  VariantAnnotation,
+  SomaticVariantAnnotation,
 } from 'app/shared/api/generated/OncoKbPrivateAPI';
 import React from 'react';
 import { Link } from 'react-router-dom';
@@ -425,6 +426,18 @@ export const DEFAULT_ONCOKB_INFO: OncoKBInfo = {
   },
   publicInstance: false,
   oncoTreeVersion: '',
+  genomeNexus: {
+    grch37: {
+      genomeNexusVepVersion: '',
+      genomeNexusVersion: '',
+      vepVersion: '',
+    },
+    grch38: {
+      genomeNexusVepVersion: '',
+      genomeNexusVersion: '',
+      vepVersion: '',
+    },
+  },
 };
 
 export const DEFAULT_MAIN_NUMBERS: MainNumber = {
@@ -456,7 +469,6 @@ export const DEFAULT_GENE_NUMBER: GeneNumber = {
   highestPrognosticImplicationLevel: '',
   highestFdaLevel: '',
   penetrance: '',
-  inheritanceMechanism: '',
   tumorType: 0,
 };
 
@@ -468,7 +480,7 @@ export const DEFAULT_MUTATION_EFFECT = {
 
   description: '',
 
-  knownEffect: '',
+  knownEffect: 'Unknown',
 };
 
 export const DEFAULT_GERMLINE_VARIANT = {
@@ -489,6 +501,7 @@ export const DEFAULT_QUERY = {
   consequence: '',
   entrezGeneId: -1,
   hgvs: '',
+  hgvsInfo: '',
   hugoSymbol: '',
   id: '',
   proteinEnd: -1,
@@ -499,6 +512,7 @@ export const DEFAULT_QUERY = {
   type: '',
   germline: false,
   alleleState: '',
+  canonicalTranscript: '',
 };
 
 const DEFAULT_ALTERATION: Alteration = {
@@ -528,7 +542,7 @@ const DEFAULT_ALTERATION: Alteration = {
   variantResidues: '',
 };
 
-export const DEFAULT_ANNOTATION: VariantAnnotation = {
+export const DEFAULT_ANNOTATION: SomaticVariantAnnotation = {
   alleleExist: false,
   alteration: DEFAULT_ALTERATION,
   background: '',
@@ -546,6 +560,7 @@ export const DEFAULT_ANNOTATION: VariantAnnotation = {
   lastUpdate: '',
   mutationEffect: DEFAULT_MUTATION_EFFECT,
   oncogenic: '',
+  exon: '',
   otherSignificantResistanceLevels: [],
   otherSignificantSensitiveLevels: [],
   prognosticImplications: [],
@@ -561,7 +576,38 @@ export const DEFAULT_ANNOTATION: VariantAnnotation = {
 
   vue: false,
   vus: false,
-  germline: DEFAULT_GERMLINE_VARIANT,
+};
+
+export const DEFAULT_GERMLINE_ANNOTATION: GermlineVariantAnnotation = {
+  alleleExist: false,
+  clinVarId: '',
+  dataVersion: '',
+  diagnosticImplications: [],
+  diagnosticSummary: '',
+  exon: '',
+  geneExist: false,
+  geneSummary: '',
+  genomicIndicators: [],
+  highestDiagnosticImplicationLevel: 'NO',
+  highestFdaLevel: 'NO',
+  highestPrognosticImplicationLevel: 'NO',
+  highestResistanceLevel: 'NO',
+  highestSensitiveLevel: 'NO',
+  lastUpdate: '',
+  mutationEffect: DEFAULT_MUTATION_EFFECT,
+  otherSignificantResistanceLevels: [],
+  otherSignificantSensitiveLevels: [],
+  pathogenic: '',
+  penetrance: '',
+  prognosticImplications: [],
+  prognosticSummary: '',
+  query: DEFAULT_QUERY,
+  treatments: [],
+  tumorTypeSummary: '',
+  tumorTypes: [],
+  variantExist: false,
+  variantSummary: '',
+  vus: false,
 };
 
 export enum PAGE_TITLE {
