ce535137a7764bb2f1c3472df7591f81c0703998
Calvin Lu <59149377+calvinlu3@users.noreply.github.com>
2026-03-04 13:01:49 -0500

    Make sure alias can be matched as well

--- src/main/webapp/app/store/AnnotationStore.ts
+++ src/main/webapp/app/store/AnnotationStore.ts
@@ -187,10 +187,19 @@ export class AnnotationStore {
         }
 
         const genes = await apiClient.genesLookupGetUsingGET({ query });
+        const normalizedQuery = query?.toUpperCase();
         const exactMatch = genes?.find(
-          gene => gene.hugoSymbol?.toUpperCase() === query?.toUpperCase()
+          gene => gene.hugoSymbol?.toUpperCase() === normalizedQuery
         );
-        return exactMatch ?? DEFAULT_GENE;
+        if (exactMatch) {
+          return exactMatch;
+        }
+        const aliasMatch = genes?.find(gene =>
+          gene.geneAliases?.some(
+            alias => alias.toUpperCase() === normalizedQuery
+          )
+        );
+        return aliasMatch ?? DEFAULT_GENE;
       } catch (e) {
         notifyError(e, 'Error finding gene');
         return DEFAULT_GENE;
