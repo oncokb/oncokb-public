518b8064be3c877041fb902fa660b9a25f03fa39
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2026-04-17 11:34:37 -0400

    Fixed grace period users not being able to log in (#1335)

--- src/main/java/org/mskcc/cbio/oncokb/web/rest/UserUUIDController.java
+++ src/main/java/org/mskcc/cbio/oncokb/web/rest/UserUUIDController.java
@@ -2,10 +2,12 @@ package org.mskcc.cbio.oncokb.web.rest;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.mskcc.cbio.oncokb.domain.Token;
+import org.mskcc.cbio.oncokb.security.SecurityUtils;
 import org.mskcc.cbio.oncokb.security.uuid.UUIDFilter;
 import org.mskcc.cbio.oncokb.security.uuid.TokenProvider;
 import org.mskcc.cbio.oncokb.service.TokenService;
 import org.mskcc.cbio.oncokb.service.UserDetailsService;
+import org.mskcc.cbio.oncokb.service.UserService;
 import org.mskcc.cbio.oncokb.service.dto.UserDetailsDTO;
 import org.mskcc.cbio.oncokb.service.dto.useradditionalinfo.Activation;
 import org.mskcc.cbio.oncokb.service.dto.useradditionalinfo.AdditionalInfoDTO;
@@ -30,6 +32,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.time.Instant;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -52,6 +55,9 @@ public class UserUUIDController {
     @Autowired
     private UserDetailsService userDetailsService;
 
+    @Autowired
+    private UserService userService;
+
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
     public UserUUIDController(TokenProvider tokenProvider, AuthenticationManagerBuilder authenticationManagerBuilder) {
@@ -68,26 +74,26 @@ public class UserUUIDController {
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
+        Optional<UserDetailsDTO> userDetails = userDetailsService.findByUserIsCurrentUser();
         List<Token> tokenList = tokenService.findByUserIsCurrentUser();
         UUID uuid;
 
-        if (tokenList.size() > 0) {
+        if (!tokenList.isEmpty()) {
             List<Token> validTokens = tokenList.stream().filter(token -> token.getExpiration().isAfter(Instant.now())).collect(Collectors.toList());
-            if (validTokens.size() > 0) {
+            if (!validTokens.isEmpty()) {
                 uuid = validTokens.iterator().next().getToken();
+            } else if (canRefreshExpiredTokenForCurrentGracePeriodUser(userDetails)) {
+                uuid = refreshExpiredTokens(tokenList);
+            } else if (tokenList.stream().noneMatch(Token::isRenewable)) {
+                throw new TrialAccountExpiredException();
             } else {
-                if (!tokenList.stream().filter(token -> token.isRenewable()).findAny().isPresent()) {
-                    throw new TrialAccountExpiredException();
-                } else {
-                    throw new TokenExpiredException();
-                }
+                throw new TokenExpiredException();
             }
         } else {
             Token token = tokenProvider.createTokenForCurrentUserLogin(Optional.empty(), Optional.empty());
             uuid = token.getToken();
         }
 
-        Optional<UserDetailsDTO> userDetails = userDetailsService.findByUserIsCurrentUser();
         if (userDetails.isPresent()) {
             UserDetailsDTO ud = userDetails.get();
             boolean userHasTrialKey = Optional.ofNullable(ud)
@@ -112,5 +118,28 @@ public class UserUUIDController {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add(UUIDFilter.AUTHORIZATION_HEADER, "Bearer " + uuid);
         return new ResponseEntity<>(uuid, httpHeaders, HttpStatus.OK);
+    }
+
+    private boolean canRefreshExpiredTokenForCurrentGracePeriodUser(Optional<UserDetailsDTO> userDetails) {
+        return userService.getUserWithAuthorities()
+            .filter(user -> !user.getActivated())
+            .flatMap(user -> userDetails
+                .map(UserDetailsDTO::getLicenseType)
+                .filter(licenseType -> SecurityUtils.isWithinActivationGracePeriod(user, licenseType)))
+            .isPresent();
+    }
+
+    private UUID refreshExpiredTokens(List<Token> tokenList) {
+        Instant defaultExpiration = Instant.now().plusSeconds(TokenProvider.EXPIRATION_TIME_IN_SECONDS);
+        tokenList.forEach(token -> {
+            Instant expirationBased = token.getExpiration().plusSeconds(TokenProvider.EXPIRATION_TIME_IN_SECONDS);
+            token.setExpiration(expirationBased.isBefore(defaultExpiration) ? defaultExpiration : expirationBased);
+            tokenService.save(token);
+        });
+
+        return tokenList.stream()
+            .max(Comparator.comparing(Token::getExpiration))
+            .map(Token::getToken)
+            .orElseThrow(TokenExpiredException::new);
     }
 }
