2cb57747795c83c7184f7f598edce4bb03c11b42
bprize15 <bprize15@gmail.com>
2026-05-15 14:53:51 -0400

    add oncokb tm to registration hover

--- src/main/webapp/app/components/registrationHover/RegistrationHover.tsx
+++ src/main/webapp/app/components/registrationHover/RegistrationHover.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import styles from './styles.module.scss';
 import classNames from 'classnames';
 import { Link, useLocation } from 'react-router-dom';
-import { PAGE_ROUTE } from 'app/config/constants';
+import { ONCOKB_TM, PAGE_ROUTE } from 'app/config/constants';
 import AuthenticationStore from 'app/store/AuthenticationStore';
 import { AppConfig } from 'app/appConfig';
 
@@ -133,7 +133,8 @@ export default function RegistrationHover({
           <p>
             Help us better understand your needs.
             <br />
-            Create an account or log in to continue accessing OncoKB content.
+            Create an account or log in to continue accessing {ONCOKB_TM}{' '}
+            content.
           </p>
           <ul>
             <li>
