7e1250404b081490dffea59e4118735ed8f7ca2c
bprize15 <bprize15@gmail.com>
2026-08-27 16:41:30 -0400

    change branch to master

--- src/main/java/org/mskcc/cbio/oncokb/web/rest/NewsResource.java
+++ src/main/java/org/mskcc/cbio/oncokb/web/rest/NewsResource.java
@@ -30,7 +30,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @RequestMapping("/api/news")
 public class NewsResource {
     
-    private static final String ONCOKB_DATA_BRANCH = "data-release-v7.5";
+    private static final String ONCOKB_DATA_BRANCH = "master";
     private static final String ONCOKB_DATA_RELEASE_FOLDER = "https://api.github.com/repos/knowledgesystems/oncokb-data/contents/RELEASE";
 
     private final ObjectMapper objectMapper;
